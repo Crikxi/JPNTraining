705b78ed635ec8583085cae4fae0bb9eb48c16fd
Crikxi <crikxi.public@gmail.com>
2020-09-25 21:04:20 +0200

    Add files via upload

---
+++ charList.js
@@ -0,0 +1,12 @@
+var charList= [
+    {name: "a", char:"あ"},
+    {name: "i", char:"い"},
+    {name: "u", char:"う"},
+    {name: "e", char:"え"},
+    {name: "o", char:"お"},
+    {name: "ka", char:"か"},
+    {name: "ki", char:"き"},
+    {name: "ku", char:"く"},
+    {name: "ke", char:"け"},
+    {name: "ko", char:"こ"}
+];
