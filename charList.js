674bedbdf9cc052dc1b4367c9e61af08cc46faa0
Crikxi <crikxi.public@gmail.com>
2020-09-27 21:09:19 +0200

    Add files via upload
    Update with fix
    Add https://commons.wikimedia.org/wiki/Category:Hiragana_stroke_order_(animated_image_set) to paint

--- charList.js
+++ charList.js
@@ -10,3 +10,20 @@ var charList= [
     {name: "ke", char:"け"},
     {name: "ko", char:"こ"}
 ];
+
+
+/*
+Next code is for false random to stop getting the sames symbols
+*/
+var remainingCharList = [];
+function getRandomChar(){
+    if(remainingCharList.length == 0)
+        remainingCharList = charList.slice(0);//Copy charList
+    
+    var i = Math.floor(Math.random()*remainingCharList.length);
+    return remainingCharList.splice(i, 1)[0];
+}
+
+function getImgLink(character){
+    return "./img/Hiragana_" + character +"_stroke_order_animation.gif"
+}
